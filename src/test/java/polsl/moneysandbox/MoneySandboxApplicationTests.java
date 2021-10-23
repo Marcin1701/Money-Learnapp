89fed7f9479ede6f615723d987e04aca1e5ba4c1
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-10-23 12:05:10 +0200

    Project Init

---
+++ src/test/java/polsl/moneysandbox/MoneySandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package polsl.moneysandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneySandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
