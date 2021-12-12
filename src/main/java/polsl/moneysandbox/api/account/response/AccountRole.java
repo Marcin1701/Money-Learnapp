9f7d6484c342945a7ec40a6e4aaed936d07fdd37
Marcin_Rogoz_Scripts-release-frontend <marcin.rogoz1@outlook.com>
2021-12-12 21:36:46 +0100

    Role

---
+++ src/main/java/polsl/moneysandbox/api/account/response/AccountRole.java
@@ -0,0 +1,15 @@
+package polsl.moneysandbox.api.account.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AccountRole {
+
+    private String role;
+}
